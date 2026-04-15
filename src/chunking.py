eb532257b2387c86870b884d015a8b5e2ad07103
Bijin Chakraborty <14847418+bijinc@users.noreply.github.com>
2026-04-14 20:12:26 -0400

    revert batch size

--- src/chunking.py
+++ src/chunking.py
@@ -16,7 +16,7 @@ SEC_OVERLAP = 160
 ECT_CHUNK_SIZE = 480
 ECT_OVERLAP = 80
 
-BATCH_SIZE = 256    # for embedding batches
+BATCH_SIZE = 128    # for embedding batches
 _SEMANTIC_EMBEDDINGS = None
 
 
